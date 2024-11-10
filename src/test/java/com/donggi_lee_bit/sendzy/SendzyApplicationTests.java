8fa6e4eadebb8eb9bc037233cb1b042998d05361
donggi-lee-bit <devdonggilee@gmail.com>
2024-11-10 22:20:28 +0900

    초기 프로젝트 설정

---
+++ src/test/java/com/donggi_lee_bit/sendzy/SendzyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.donggi_lee_bit.sendzy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SendzyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
