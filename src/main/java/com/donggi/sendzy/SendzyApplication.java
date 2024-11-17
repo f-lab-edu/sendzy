1af376bf1379b0ae038ca1fb88b9588d120182d3
donggi-lee-bit <devdonggilee@gmail.com>
2024-11-17 19:41:14 +0900

    패키지에서 언더바를 제거하기 위해 네이밍 변경

--- src/main/java/com/donggi/sendzy/SendzyApplication.java
+++ src/main/java/com/donggi/sendzy/SendzyApplication.java
@@ -1,4 +1,4 @@
-package com.donggi_lee_bit.sendzy;
+package com.donggi.sendzy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
