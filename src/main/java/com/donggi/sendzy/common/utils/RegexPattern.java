3f3f0417d2a54a0e8f133ea35c02c7845122017c
donggi-lee-bit <devdonggilee@gmail.com>
2024-11-24 22:47:42 +0900

    RegexPattern enum 클래스에 @Getter 적용

--- src/main/java/com/donggi/sendzy/common/utils/RegexPattern.java
+++ src/main/java/com/donggi/sendzy/common/utils/RegexPattern.java
@@ -1,5 +1,8 @@
 package com.donggi.sendzy.common.utils;
 
+import lombok.Getter;
+
+@Getter
 public enum RegexPattern {
     EMAIL("^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,6}$", "유효한 이메일 형식 (예: user@example.com)"),
     PASSWORD("^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*[!@#$%^&*]).{8,}$", "영문 대문자, 영문 소문자, 숫자, 특수문자 중 3가지 이상 포함");
@@ -10,13 +13,5 @@ public enum RegexPattern {
     RegexPattern(String pattern, String description) {
         this.pattern = pattern;
         this.description = description;
-    }
-
-    public String getPattern() {
-        return pattern;
-    }
-
-    public String getDescription() {
-        return description;
     }
 }
